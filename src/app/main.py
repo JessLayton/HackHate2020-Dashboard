01398b23ce33f73b8d0078a5059355db092ceda2
Dylan Evans <dylanlee.evans95@gmail.com>
2021-06-11 20:48:05 +0100

    Rename view functions

--- src/app/main.py
+++ src/app/main.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 
 @app.route("/reportingNumbers", methods=['POST'])
-def index() -> str:
+def reportingNumbers() -> str:
     # transform a dict into an application/json response 
     return {"message": "Reporting numbers works"}
 
 @app.route("/unreportedCases", methods=['POST'])
-def index() -> str:
+def unreportedCases() -> str:
     # transform a dict into an application/json response 
     return {"message": "Unreported cases works"}
 
