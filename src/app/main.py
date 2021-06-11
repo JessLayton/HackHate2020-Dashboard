b150fbf185b754246dd673420594ceb47d37ad35
Dylan Evans <dylanlee.evans95@gmail.com>
2021-06-11 20:46:10 +0100

    Remove jsonify from main.py
    Dicts returned from view functions are automatically
    transformed to json.

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,12 +6,12 @@ app = Flask(__name__)
 @app.route("/reportingNumbers", methods=['POST'])
 def index() -> str:
     # transform a dict into an application/json response 
-    return jsonify({"message": "Reporting numbers works"})
+    return {"message": "Reporting numbers works"}
 
 @app.route("/unreportedCases", methods=['POST'])
 def index() -> str:
     # transform a dict into an application/json response 
-    return jsonify({"message": "Unreported cases works"})
+    return {"message": "Unreported cases works"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
