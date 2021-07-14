1bbd8407ee5aa3de14e0e03f73203d54051c44b0
Dylan Evans <dylanlee.evans95@gmail.com>
2021-07-14 16:38:18 +0100

    Add logic to unreportedCases endpoint

--- src/app/main.py
+++ src/app/main.py
@@ -46,7 +46,29 @@ def reporting_numbers() -> str:
 def unreported_cases() -> str:
     '''
     Return a dict to be used by the dashboard to display the data.
+
+    Uses the JSend format.
     '''
+    try:
+        # Validate request JSON
+        data = request.get_json()
+        success, message = validators.validate_reporting_numbers(data)
+        # If request JSON is invalid, return a failure message.
+        if not success:
+            return {"status": "fail", "message": message}
+
+        quarters = []
+
+        for quarter_data in sorted(data['body'], key=lambda x: (x['year'], x['quarter')]:
+            quarter_text = f"Q{quarter_data['quarter'] quarter_data['year']"
+            quarter = {"quarter": quarter_text}
+            quarter.update(quarter_data['unreportedCases'])
+            quarters.append(quarter)
+    except Exception as err:
+        return {"status": "error", "message": repr(err)}
+
+    return {"status", "success", "data": quarters}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
